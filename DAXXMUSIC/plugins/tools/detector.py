44dd5cc328a4f37e5c9d681568d0127f98d246df
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-07-18 20:48:26 +0530

    Update detector.py

--- DAXXMUSIC/plugins/tools/detector.py
+++ DAXXMUSIC/plugins/tools/detector.py
@@ -1,14 +1,16 @@
 import logging
+from telegram.ext import Updater, CommandHandler
 from DAXXMUSIC import app
 
 class ActiveMembersDetector:
-    def init(self, token):
+    def __init__(self, token):
         self.token = token
         self.updater = Updater(token, use_context=True)
         self.dp = self.updater.dispatcher
 
         self.dp.add_handler(CommandHandler('start', self.start))
         self.dp.add_handler(CommandHandler('active', self.detect_active_members))
+        self.dp.add_handler(CommandHandler('detect', self.detect_command))
 
     def start(self, update, context):
         context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! I\'m active member detector bot.')
@@ -19,14 +21,20 @@ class ActiveMembersDetector:
         active_members = 0
 
         for member in context.bot.get_chat_members(chat_id):
-            if member.status in ['creator', 'administrator', 'ember']:
+            if member.status in ['creator', 'administrator', 'member']:
                 active_members += 1
 
         context.bot.send_message(chat_id=update.effective_chat.id, text=f'Active members: {active_members}/{members}')
+
+    def detect_command(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Running active members detection...')
+        self.detect_active_members(update, context)
 
     def run(self):
         self.updater.start_polling()
         self.updater.idle()
 
-def create_active_members_detector(token):
-    return ActiveMembersDetector(token)
+def main():
+    token = '7473830088:AAFUvpAti-vlZMG7ADwK8toY199W1OZh4cc'
+    detector = ActiveMembersDetector(token)
+    detector.run()

0a381cd12c5545daa2a5d67aaed6e2cd9a066e1a
SkOfficial01 <123543112+SkOfficial01@users.noreply.github.com>
2024-07-18 20:36:50 +0530

    Create detector.py

---
+++ DAXXMUSIC/plugins/tools/detector.py
@@ -0,0 +1,32 @@
+import logging
+from DAXXMUSIC import app
+
+class ActiveMembersDetector:
+    def init(self, token):
+        self.token = token
+        self.updater = Updater(token, use_context=True)
+        self.dp = self.updater.dispatcher
+
+        self.dp.add_handler(CommandHandler('start', self.start))
+        self.dp.add_handler(CommandHandler('active', self.detect_active_members))
+
+    def start(self, update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text='Hello! I\'m active member detector bot.')
+
+    def detect_active_members(self, update, context):
+        chat_id = update.effective_chat.id
+        members = context.bot.get_chat_members_count(chat_id)
+        active_members = 0
+
+        for member in context.bot.get_chat_members(chat_id):
+            if member.status in ['creator', 'administrator', 'ember']:
+                active_members += 1
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=f'Active members: {active_members}/{members}')
+
+    def run(self):
+        self.updater.start_polling()
+        self.updater.idle()
+
+def create_active_members_detector(token):
+    return ActiveMembersDetector(token)
